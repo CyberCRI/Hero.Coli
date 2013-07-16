d06105fb919366bc484cb4ea421e017616e7e34c
yleflour <moufoo@gmail.com>
2013-07-16 11:33:00 +0200

    Plugins Added

---
+++ Assets/NGUI/Scripts/Tweening/TweenFOV.cs
@@ -0,0 +1,59 @@
+﻿//----------------------------------------------
+//            NGUI: Next-Gen UI kit
+// Copyright © 2011-2012 Tasharen Entertainment
+//----------------------------------------------
+
+using UnityEngine;
+
+/// <summary>
+/// Tween the camera's field of view.
+/// </summary>
+
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("NGUI/Tween/Field of View")]
+public class TweenFOV : UITweener
+{
+	public float from;
+	public float to;
+
+	Camera mCam;
+
+	/// <summary>
+	/// Camera that's being tweened.
+	/// </summary>
+
+	public Camera cachedCamera { get { if (mCam == null) mCam = camera; return mCam; } }
+
+	/// <summary>
+	/// Current field of view value.
+	/// </summary>
+
+	public float fov { get { return cachedCamera.fov; } set { cachedCamera.fov = value; } }
+
+	/// <summary>
+	/// Perform the tween.
+	/// </summary>
+
+	override protected void OnUpdate (float factor, bool isFinished)
+	{
+		cachedCamera.fov = from * (1f - factor) + to * factor;
+	}
+
+	/// <summary>
+	/// Start the tweening operation.
+	/// </summary>
+
+	static public TweenFOV Begin (GameObject go, float duration, float to)
+	{
+		TweenFOV comp = UITweener.Begin<TweenFOV>(go, duration);
+		comp.from = comp.fov;
+		comp.to = to;
+
+		if (duration <= 0f)
+		{
+			comp.Sample(1f, true);
+			comp.enabled = false;
+		}
+		return comp;
+	}
+}
