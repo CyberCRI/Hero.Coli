25e855731f44408c9aafe5c2932d49211346d689
martingoldschmid <martingoldschmid@cri43.medecine.univ-paris5.fr>
2013-07-19 17:29:38 +0200

    Merge armella martin works merge number 23

---
+++ Assets/Graphics/MeshSamples/Animation/slowDown.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class slowDown : MonoBehaviour {
+	
+	public string animName;
+	public float percentage = 50;
+	
+	void Update(){
+		percentage = Mathf.Clamp(percentage, 0f, 500f);
+		animation[animName].speed = percentage / 100f;
+	}
+}
