c15cc2ffaa113a362042c8b5937b3f5a68c0b24d
raphael goujet <raphael.goujet@gmail.com>
2013-07-26 14:56:39 +0200

    fixed failed merge

--- Assets/Scripts/ReactionEngine/Promoter.cs
+++ Assets/Scripts/ReactionEngine/Promoter.cs
@@ -6,13 +6,18 @@ using UnityEngine;
 
 public class PromoterProprieties
 {
-  public string name;
-  public float beta;
-  public float terminatorFactor;
-  public string formula;
-  public LinkedList<Product> products;
-  public float energyCost;
+  public string name;                           //!< The name of the reaction
+  public float beta;                            //!< The maximal production rate of the promoter
+  public float terminatorFactor;                //!< The Coefficient that represent the terminator
+  public string formula;                        //!< The formula that drive the promoter behaviour
+  public LinkedList<Product> products;          //!< The list of products
+  public float energyCost;                      //!< The cost in energy
 	
+	
+/*!
+ *  \brief     ToString method
+ *  \details   ToString method, with all fields, including detailed internal products
+ */
   public override string ToString() {
 	string productsString = "Products[";
 	IEnumerator<Product> enumerator = products.GetEnumerator();
