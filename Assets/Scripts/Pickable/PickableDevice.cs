73784cad74bd9626db0f5c17f8424c3b3ec286d3
raphael goujet <raphael.goujet@gmail.com>
2013-12-05 17:27:31 +0100

    issue #8: refactored device pick-up to facilitate integration

--- Assets/Scripts/Pickable/PickableDevice.cs
+++ Assets/Scripts/Pickable/PickableDevice.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class PickableDevice : PickableItem {
+public abstract class PickableDevice : PickableItem {
   public DevicesDisplayer _devicesDisplayer;
   public AvailableBioBricksManager _availableBioBricksManager;
   protected new Device _dnaBit;
@@ -10,14 +10,10 @@ public class PickableDevice : PickableItem {
   void Awake()
   {
     Logger.Log("PickableDevice::Start", Logger.Level.TEMP);
-    PromoterBrick prom = new PromoterBrick("PromY2", 75f, "[0.01,2]Y");
-    RBSBrick rbs = new RBSBrick("RBS3", 3.0f);
-    GeneBrick gene = new GeneBrick("X", "X");
-    TerminatorBrick term = new TerminatorBrick("T1", 1.0f);
-    LinkedList<BioBrick> bricks = new LinkedList<BioBrick>(new List<BioBrick>(){prom, rbs, gene, term});
-    ExpressionModule module = new ExpressionModule("expr", bricks);
-    _dnaBit = Device.buildDevice("DEV", new LinkedList<ExpressionModule>(new List<ExpressionModule>(){module}));
+    _dnaBit = produceDevice();
   }
+
+  protected abstract Device produceDevice();
 
   protected override void addTo()
   {
