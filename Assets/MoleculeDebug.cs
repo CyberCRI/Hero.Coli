338da7f5c40fbcf447af486494ad8bae331ac9ba
raphael goujet <raphael.goujet@gmail.com>
2014-01-15 14:56:53 +0100

    fixed #155 graph: molecule name flickering.

--- Assets/MoleculeDebug.cs
+++ Assets/MoleculeDebug.cs
@@ -3,11 +3,64 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class MoleculeDebug : MonoBehaviour {
-	
+
 	private ReactionEngine   _reactionEngine;
 	public int               mediumId;
-	public UILabel           label;
+	public UILabel           namesLabel;
+  public UILabel           valuesLabel;
 	public bool              displayAll;
+
+  private LinkedList<DebugMolecule> _debugMolecules = new LinkedList<DebugMolecule>();
+  private LinkedList<DebugMolecule> _toRemove = new LinkedList<DebugMolecule>();
+
+  private class DebugMolecule
+  {
+    private string _name;
+    private string _val;
+    private bool _updated;
+
+    public string getName()
+    {
+      return _name;
+    }
+
+    public string getVal()
+    {
+      return _val;
+    }
+
+    public bool isUpdated()
+    {
+      return _updated;
+    }
+
+    public DebugMolecule(string name, string val)
+    {
+      _updated = true;
+      _name = name;
+      _val = val;
+    }
+
+    public DebugMolecule(string name, float val) : this(name, val.ToString())
+    {
+    }
+
+    public void update(string val)
+    {
+      _val = val;
+      _updated = true;
+    }
+
+    public void update(float val)
+    {
+      update(val.ToString());
+    }
+
+    public void reset()
+    {
+      _updated = false;
+    }
+  }
 
   public void setMediumId(int newMediumId)
   {
@@ -18,26 +71,69 @@ public class MoleculeDebug : MonoBehaviour {
     _reactionEngine = ReactionEngine.get();
   }
 
+  private void resetMoleculeList()
+  {
+    foreach(DebugMolecule molecule in _debugMolecules)
+    {
+      molecule.reset();
+    }
+  }
+
+  private void removeUnusedMolecules()
+  {
+    _toRemove.Clear();
+    foreach(DebugMolecule molecule in _debugMolecules)
+    {
+      if(!molecule.isUpdated())
+      {
+        _toRemove.AddLast(molecule);
+      }
+    }
+    foreach(DebugMolecule molecule in _toRemove)
+    {
+      _debugMolecules.Remove(molecule);
+    }
+
+  }
+
 	// Update is called once per frame
 	void Update () {
-		string inter = null;
+
+    resetMoleculeList();
+
 		ArrayList molecules = _reactionEngine.getMoleculesFromMedium(mediumId);
-		List<string> llMolecules = new List<string>();
 		foreach(System.Object molecule in molecules) {
-			inter = ((Molecule)molecule).ToShortString(displayAll);
-			if(!string.IsNullOrEmpty(inter)) {
-				llMolecules.Add(inter);
-			}
+      Molecule castMolecule = (Molecule)molecule;
+      string name = castMolecule.getRealName();
+			float concentration = castMolecule.getConcentration();
+      if(displayAll || (0 != concentration))
+      {
+        DebugMolecule found = LinkedListExtensions.Find(_debugMolecules, m => m.getName() == name);
+        if(null != found)
+        {
+          found.update(concentration);
+        }
+        else
+        {
+          DebugMolecule created = new DebugMolecule(name, concentration);
+          _debugMolecules.AddLast(created);
+        }
+      }
 		}
-		llMolecules.Sort();
+
+    removeUnusedMolecules();
 		
-		string toDisplay = "";
-		foreach(string molecule in llMolecules) {
-			toDisplay+=molecule+"\n";
+		string namesToDisplay = "";
+    string valuesToDisplay = "";
+		foreach(DebugMolecule molecule in _debugMolecules) {
+			namesToDisplay+=molecule.getName()+":\n";
+      valuesToDisplay+=molecule.getVal()+"\n";
 		}
-		if(!string.IsNullOrEmpty(toDisplay)) {
-			toDisplay.Remove(toDisplay.Length-1, 1);
-		}	
-		label.text = toDisplay;	
+		if(!string.IsNullOrEmpty(namesToDisplay)) {
+			namesToDisplay.Remove(namesToDisplay.Length-1, 1);
+      valuesToDisplay.Remove(valuesToDisplay.Length-1, 1);
+		}
+    namesLabel.text = namesToDisplay;
+    valuesLabel.text = valuesToDisplay;
 	}
 }
