056bb81062c6713beb471b7ae4f8aa8d32194c31
raphael goujet <raphael.goujet@gmail.com>
2014-01-17 17:34:02 +0100

    fixed #165 Pushable rocks

--- Assets/Scripts/Puzzles/PushableBox.cs
+++ Assets/Scripts/Puzzles/PushableBox.cs
@@ -6,31 +6,54 @@ public class PushableBox : MonoBehaviour {
 	
 	public float minSpeed;
 	private Vector3 _initPos;
+  private CellControl _control = null;
+  private RigidbodyConstraints noPush = RigidbodyConstraints.FreezeAll;
+  private RigidbodyConstraints canPush = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
 	
 	void Start(){
 		_initPos = transform.position;
 	}
-	
-    void OnCollisionEnter(Collision col) {
-		if (col.collider){
-			CellControl cc = col.gameObject.GetComponent<CellControl>();
-			if(cc && cc.currentMoveSpeed >= minSpeed){
-				rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
-			}
-		}
-	}
-	
-	void OnCollisionExit(Collision col) {
-		if (col.collider){
-			CellControl cc = col.gameObject.GetComponent<CellControl>();
-			if(cc)
-				rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-		}
-		
-	}
+
+  private CellControl lazySafeGetCellControl(Collision col)
+  {
+    if(null == _control)
+    {
+      _control = col.gameObject.GetComponent<CellControl>();
+    }
+    return _control;
+  }
+
+  void OnCollisionEnter(Collision col) {
+    if (col.collider){
+      lazySafeGetCellControl(col);
+      if(_control && _control.currentMoveSpeed >= minSpeed){
+        rigidbody.constraints = canPush;
+      }
+    }
+  }
+  
+  void OnCollisionExit(Collision col) {
+    if (col.collider){
+      lazySafeGetCellControl(col);
+      if(_control)
+        rigidbody.constraints = noPush;
+    }
+  }
+
+  void OnCollisionStay(Collision col)
+  {
+    if(rigidbody.constraints != canPush)
+    {
+      if (col.collider){
+        lazySafeGetCellControl(col);
+        if(_control && _control.currentMoveSpeed >= minSpeed){
+          rigidbody.constraints = canPush;
+        }
+      }
+    }
+  }
 	
 	public void resetPos(){
-		Debug.Log("WTF");
 		transform.position = _initPos;
 	}
 	
