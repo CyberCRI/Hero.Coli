0def9275986125dce4234e2be9349d7050d6ff3d
raphael goujet <raphael.goujet@gmail.com>
2014-02-12 10:39:47 +0100

    script to restrain motion of bacterium

--- Assets/Scripts/StandaloneLevels/StandaloneLevel_001/MotionRestrainer.cs
+++ Assets/Scripts/StandaloneLevels/StandaloneLevel_001/MotionRestrainer.cs
@@ -2,24 +2,21 @@ using UnityEngine;
 
 public class MotionRestrainer : MonoBehaviour
 {
-  private float ratio = 0.95f;
-  static private float radius = 10f;
+  static private float radius = 50f;
   static private float sqrRadius = radius*radius;
   private Vector3 _initialPosition;
-  //private CellControl _cellControl;
 
   void Start()
   {
     _initialPosition = transform.position;
-    //_cellControl = GameObject.Find("Perso").GetComponent<CellControl>();
   }
 
   void Update()
   {
-    Vector3 deltaPosition = _initialPosition - transform.position;
-    if((_initialPosition - transform.position).sqrMagnitude >= sqrRadius)
+    Vector3 deltaPosition = transform.position - _initialPosition;
+    if(deltaPosition.sqrMagnitude > sqrRadius)
     {
-      Vector3 newPosition = deltaPosition.normalized*ratio*radius + _initialPosition;
+      Vector3 newPosition = deltaPosition.normalized*radius + _initialPosition;
       transform.position = newPosition;
     }
   }
