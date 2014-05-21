e5c96f9d497974f1e3f0ff1fbcfcb54d5b5ba0ff
raphael goujet <raphael.goujet@gmail.com>
2014-05-21 12:49:32 +0200

    #117 the Click to Move is back, but it's all out of gum

--- Assets/Scripts/Charac/CellControl.cs
+++ Assets/Scripts/Charac/CellControl.cs
@@ -9,10 +9,23 @@ public class CellControl : MonoBehaviour{
 	public List<Animation> anims;
   public Hero hero;
   public float moveEnergyCost;
-
   public float currentMoveSpeed;
 
   private bool _pause;
+
+  //Click to move variables    
+  private int _smooth; // Determines how quickly object moves towards position
+  private float _hitdist = 0.0f;
+  private Vector3 _targetPosition;
+    
+  private enum ControlType {
+      RightClickToMove,
+      LeftClickToMove,
+      AbsoluteWASD,
+      RelativeWASD
+  };
+  private ControlType _currentControlType = ControlType.LeftClickToMove;
+
 
   public void Pause(bool pause)
   {
@@ -22,44 +35,76 @@ public class CellControl : MonoBehaviour{
   public bool isPaused()
   {
     return _pause;
+    }
+
+  private void ClickToMoveUpdate(KeyCode mouseButtonCode) {
+    if(Input.GetKeyDown(mouseButtonCode))            
+    {
+        _smooth = 1;
+        
+        Plane playerPlane = new Plane(Vector3.up, transform.position);            
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);            
+        
+        if (playerPlane.Raycast (ray, out _hitdist)) {                
+            _targetPosition = ray.GetPoint(_hitdist);     
+            transform.rotation = Quaternion.LookRotation(_targetPosition - transform.position);
+        }
+    }
+    transform.position = Vector3.Slerp (transform.position, _targetPosition, Time.deltaTime * _smooth);    
+  }
+    
+  private void AbsoluteWASDUpdate() {
+    if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+      //Rotation
+      float rotation = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
+      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(rotation, Vector3.up), Time.deltaTime * rotationSpeed);
+            
+      //Translate
+      Vector3 inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+      if(inputMovement.sqrMagnitude > 1) inputMovement /= Mathf.Sqrt(2);
+      Vector3 moveAmount = inputMovement * currentMoveSpeed;
+            
+      this.collider.attachedRigidbody.AddForce(moveAmount);
+            
+      float cost = moveAmount.sqrMagnitude*moveEnergyCost;
+      //Logger.Log ("sqrInputMovementMagnitude="+inputMovement.sqrMagnitude, Logger.Level.ONSCREEN);
+      hero.subEnergy(cost);
+            
+      //SetSpeed
+      float speed = Mathf.Abs(Vector2.Distance(Vector2.zero, new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")))) + 0.3f;
+      Animation[] anims = GetComponentsInChildren<Animation>();
+      foreach(Animation anim in anims) {
+        foreach (AnimationState state in anim) {
+          state.speed = speed;
+        }
+      }  
+    }
   }
 	
-	//Getter & setter for the move speed.
-	
-	//References for the click to move controler.
-	//	private float _moveSmooth = .5f;
-	//	private Vector3 _destination = Vector3.zero;
-
-	//Getter & setter for the inventory
 	void Start (){
     gameObject.GetComponent<PhenoSpeed>().setBaseSpeed(baseMoveSpeed);
 	}
   
 	void Update(){
 		//Keyboard controls
-		if(!_pause && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)){
-			//Rotation
-			float rotation = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(rotation, Vector3.up), Time.deltaTime * rotationSpeed);
-			
-			//Translate
-			Vector3 inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-      if(inputMovement.sqrMagnitude > 1) inputMovement /= Mathf.Sqrt(2);
-      Vector3 moveAmount = inputMovement * currentMoveSpeed;
-
-			this.collider.attachedRigidbody.AddForce(moveAmount);
-
-      float cost = moveAmount.sqrMagnitude*moveEnergyCost;
-      //Logger.Log ("sqrInputMovementMagnitude="+inputMovement.sqrMagnitude, Logger.Level.ONSCREEN);
-      hero.subEnergy(cost);
-
-			//SetSpeed
-			float speed = Mathf.Abs(Vector2.Distance(Vector2.zero, new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")))) + 0.3f;
-			Animation[] anims = GetComponentsInChildren<Animation>();
-			foreach(Animation anim in anims)
-        foreach (AnimationState state in anim) {
-          state.speed = speed;
-        }
-	  	}
+		if(!_pause) {
+      switch(_currentControlType) {
+        case ControlType.LeftClickToMove:
+          ClickToMoveUpdate(KeyCode.Mouse0);
+          break;
+        case ControlType.RightClickToMove:
+          ClickToMoveUpdate(KeyCode.Mouse1);
+          break;
+        case ControlType.AbsoluteWASD:
+          AbsoluteWASDUpdate();
+          break;
+        case ControlType.RelativeWASD:
+          AbsoluteWASDUpdate();
+          break;
+        default:
+          AbsoluteWASDUpdate();
+          break;
+      }
     }
+  }
 }
