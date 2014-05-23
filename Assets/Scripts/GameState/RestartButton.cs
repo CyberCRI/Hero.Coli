41d18e0387b71274132cd847e2bbbfd84664c62f
raphael goujet <raphael.goujet@gmail.com>
2014-05-23 17:18:20 +0200

    #202 made the restart button work but bug: biobricks from previous session are kept

---
+++ Assets/Scripts/GameState/RestartButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartButton : MonoBehaviour {    
+  void OnPress(bool isPressed) {
+    if(isPressed) {
+      Logger.Log("RestartButton::OnPress()", Logger.Level.INFO);
+      Application.LoadLevel("Master-Demo-0.1");
+    }
+  }
+}
