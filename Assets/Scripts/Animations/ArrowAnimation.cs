a9db1ee86f9f8bf94b6641b7679aa6f47762acc0
Vincent-Alexandrine <vincent.alexand@gmail.com>
2014-06-12 15:19:09 +0200

    Add tutorial pointers, indicating when there is a new device.

---
+++ Assets/Scripts/Animations/ArrowAnimation.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowAnimation : MonoBehaviour {
+
+	bool playing = false;
+	bool toswitch = false; // say if the animation have to be launch  next update()
+	GameObject clone;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void CreateAnim() {
+	
+		toswitch = true;
+		playing = false;
+	}
+
+	public void Play() {
+		if (playing == false)
+		{
+			if(GUITransitioner.get()._worldScreen.activeInHierarchy)
+			{
+				Create();
+				toswitch = false;
+				playing = true;
+			}
+			else
+			{
+
+				GUITransitioner.get().arrowManager.waitingAnim +=1;
+				toswitch = true;
+				//playing = true;
+
+			}
+		}
+		else 
+		{
+			Destroy(clone);
+			playing = false;
+		}
+
+	}
+
+	private void Create() {
+
+		Vector3 targetVector;
+
+		GameObject g = GameObject.Find("WorldEquipButtonPanel");
+
+
+
+
+		targetVector = g.transform.FindChild("WorldEquipButton").localPosition;
+
+		//Clone the arrow
+		clone = NGUITools.AddChild(g,this.gameObject);
+		clone.SetActive(true);
+
+		//GUITransitioner.get().arrowManager.arrowList.AddLast(clone.GetComponent<ArrowAnimation>());
+
+		//Bounds sizeOfChild = g.transform.FindChild("Background").renderer.bounds;
+		clone.transform.localPosition = new Vector3(targetVector.x,targetVector.y+50,targetVector.z);
+
+
+	}
+
+	public void ToggleSwitch ()
+	{
+		toswitch = !toswitch;
+	}
+}
---
+++ Assets/Scripts/Animations/ArrowAnimationManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArrowAnimationManager : MonoBehaviour {
+
+	public  LinkedList<ArrowAnimation> arrowList;
+
+	public int waitingAnim = 0;
+	
+	// Use this for initialization
+	void Start () {
+
+		arrowList = new LinkedList<ArrowAnimation>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void launchAnim ()
+	{
+		if ( waitingAnim > 0 && GUITransitioner.get()._worldScreen.activeInHierarchy)
+		{
+			for(int i=0 ; i< waitingAnim ; i++)
+			{
+				Inventory.get().scriptAnimator.arrowTuto.Play();
+				waitingAnim-=1;
+			}
+		}
+	}
+}
