343277e8436c8f28ce25d06afae80555c8c6a6d3
Vincent-Alexandrine <vincent.alexand@gmail.com>
2014-06-19 15:59:22 +0200

    Instruction Panel : start

---
+++ Assets/Scripts/Tutorial/QueryButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QueryButtonManager : MonoBehaviour {
+
+
+	public GameObject mediumInfoInstruction;
+	public GameObject queryButton;
+
+	// Use this for initialization
+	void Start () {
+		Logger.Log("QueryMnager start",Logger.Level.WARN);
+		GameObject clone = NGUITools.AddChild(GameObject.Find("MediumInfoPanelRoom"),queryButton);
+		clone.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
