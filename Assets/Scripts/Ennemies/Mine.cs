8c3b6857842e895a18864cfd848f9d526fb668f2
Vincent-Alexandrine <vincent.alexand@gmail.com>
2014-07-10 17:46:14 +0200

    Mine detection added

--- Assets/Scripts/Ennemies/Mine.cs
+++ Assets/Scripts/Ennemies/Mine.cs
@@ -6,15 +6,13 @@ public class Mine : MonoBehaviour {
 
 	private MeshRenderer _physicRenderer;
 	private MeshRenderer _lightRenderer;
-	private float _radius = 5f;
-	private bool _found = false;
+	private float _radius = 10f;
 	// Use this for initialization
 	void Start () {
 
 		_physicRenderer = transform.FindChild("Mine Physic Collider").GetComponent<MeshRenderer>();
 		_physicRenderer.enabled = false;
 
-		Logger.Log ("_physicrenderer enabled ::"+_physicRenderer.enabled,Logger.Level.WARN);
 		_lightRenderer = transform.FindChild("Mine Light Collider").GetComponent<MeshRenderer>();
 		_lightRenderer.enabled = false;
 
@@ -28,32 +26,24 @@ public class Mine : MonoBehaviour {
 
 	void detection() {
 
-		if(_found)
-			_found = false;
 
-		Collider[] hitColliders = Physics.OverlapSphere(transform.position, _radius);
-		foreach(Collider c in hitColliders)
-		{
-			Logger.Log ("name::"+c.gameObject.name,Logger.Level.WARN);
-			if (c.gameObject.name == "Perso" && _lightRenderer.enabled == false)
-			{
-				_lightRenderer.enabled = true;
-				break;
-				_found= true;
-			}
-		}
-
-	/*	Collider[] hitsColliders = Physics.OverlapSphere(transform.position, _radius);
+		Collider[] hitsColliders = Physics.OverlapSphere(transform.position, _radius);
 		
 		// If Player is in a small area
-		if (System.Array.FindAll(hitsColliders, (col) => {return (col.gameObject.name == "Perso");}))
+		if (System.Array.Find(hitsColliders, (col) => col.gameObject.name == "Perso"))
 		{
-			Logger.Log ("trouvé",Logger.Level.WARN);
-		}*/
-
-
-		if(_found == false && _lightRenderer.enabled == true)
+			if(!_lightRenderer.enabled)
+			{
+				_physicRenderer.enabled = true;
+				_lightRenderer.enabled = true;
+			}
+		}
+		else if(_lightRenderer.enabled == true)
+		{
+			_physicRenderer.enabled = false;
 			_lightRenderer.enabled = false;
+		}
+
 
 	}
 }
