9f9adb3ef1bed6f42aca2e78b8a9851fcbe37523
raphael goujet <raphael.goujet@gmail.com>
2014-09-04 17:47:59 +0200

    improved the restart button - issue #148

---
+++ Assets/Scripts/Buttons/ScaleLabelLengthMatcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleLabelLengthMatcher : MonoBehaviour {
+
+  private int _padding = 10;
+  private UILabel _label;
+  private UISprite _bg;
+  private BoxCollider _collider;
+
+	// Use this for initialization
+  void Start () {        
+    _label = gameObject.GetComponentInChildren<UILabel>();        
+    _bg = gameObject.GetComponentInChildren<UISprite>();
+    _collider = gameObject.GetComponent<BoxCollider>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    _bg.transform.localScale = new Vector3(
+            _label.relativeSize.x*_label.transform.localScale.x+_padding, 
+            _bg.transform.localScale.y, 
+            _bg.transform.localScale.z);   
+
+    _collider.size = _bg.transform.localScale;
+	}
+}
