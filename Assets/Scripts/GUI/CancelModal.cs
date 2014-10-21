a9ecd8a03b2c5e2fe281d0220212c50d3dcb79e6
raphael goujet <raphael.goujet@gmail.com>
2014-10-21 12:38:43 +0200

    fixed Cancel button in the generic modal window

--- Assets/Scripts/GUI/CancelModal.cs
+++ Assets/Scripts/GUI/CancelModal.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class QuitModalWindow : ModalButton {
-  protected override void OnPress(bool isPressed) {
+public class CancelModal : MonoBehaviour {
+  void OnPress(bool isPressed) {
     if(isPressed) {
-      Logger.Log("QuitModalWindow::OnPress()", Logger.Level.INFO);
+      Logger.Log("CancelModal::OnPress()", Logger.Level.INFO);
       GameStateController.get().changeState(GameState.Game);
       GameStateController.get().dePauseForbidden = false;
+      ModalManager.get().unsetModal();
     }
-    base.OnPress(isPressed);
   }
 }
--- Assets/Scripts/GameState/ModalRestart.cs
+++ Assets/Scripts/GameState/ModalRestart.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ModalRestart : ModalButton {
   protected override void OnPress(bool isPressed) {
     if(isPressed) {
-      Logger.Log("RestartButton::OnPress()", Logger.Level.INFO);
+      Logger.Log("ModalRestart::OnPress()", Logger.Level.INFO);
       GameStateController.restart();
     }
     //FIXME usefulness?
