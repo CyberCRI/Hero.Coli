e9baf1a49a23a7b31a013d80fc56e11642e1c14f
raphael goujet <raphael.goujet@gmail.com>
2015-01-10 15:42:53 +0100

    fixed bottom left restart button

---
+++ Assets/Scripts/Buttons/GenericDisplayModalButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericDisplayModalButton : ModalButton
+{
+    public bool unsetModal;
+    public string modalCode;
+
+    protected override void OnPress (bool isPressed)
+    {
+        if (isPressed) {
+            Logger.Log ("GenericDisplayModalButton::OnPress() with modalCode=" + modalCode, Logger.Level.INFO);
+            
+            //TODO manage stack of modal elements in ModalManager
+            if (unsetModal) {
+                ModalManager.unsetModal ();
+            }
+            ModalManager.setModal (modalCode);
+        }
+    }
+}
