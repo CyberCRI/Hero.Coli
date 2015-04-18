b16578918cf402dfb49ea6401ac58ad5b924737e
raphael goujet <raphael.goujet@gmail.com>
2015-04-18 05:32:14 +0200

    added interactions with Redmetrics tracking system

--- Assets/Scripts/GameState/MemoryManager.cs
+++ Assets/Scripts/GameState/MemoryManager.cs
@@ -30,6 +30,11 @@ public class MemoryManager : MonoBehaviour {
         Logger.Log("MemoryManager::Awake", Logger.Level.DEBUG);
         MemoryManager.get ();
     }
+
+    void Start ()
+    {
+        createPlayer (www => trackStart(www));
+    }
     ////////////////////////////////////////////////////////////////////////////////////////////
     
     public string[] inputFiles;
@@ -128,5 +133,91 @@ public class MemoryManager : MonoBehaviour {
     void OnDestroy()
     {
         Logger.Log("MemoryManager::OnDestroy", Logger.Level.DEBUG);
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    ///REDMETRICS TRACKING ///////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private string redMetricsURL = "https://api.redmetrics.io/v1/";
+    private string redMetricsEvent = "event";
+    private string redMetricsPlayer = "player";
+    private string gameVersion = "\"99a00e65-6039-41a3-a85b-360c4b30a466\"";
+    private string playerID = "\"b5ab445a-56c9-4c5b-a6d0-86e8a286cd81\"";
+    
+    private string createPlayerEventType = "\"newPlayer\"";
+    private string startEventType = "\"start\"";
+    
+    void setPlayerID (string pID)
+    {
+        Debug.Log("setPlayerID("+pID+")");
+        playerID = pID;
+    }
+    
+    private void createEvent (string eventType)
+    {
+        string url = redMetricsURL + redMetricsEvent;
+        Dictionary<string, string> headers = new Dictionary<string, string> ();
+        headers.Add ("Content-Type", "application/json");
+        
+        string ourPostData = "{\"gameVersion\":" + gameVersion + "," +
+            "\"player\":" + playerID + "," +
+                "\"type\":"+eventType+"}";
+        byte[] pData = System.Text.Encoding.ASCII.GetBytes (ourPostData.ToCharArray ());
+        Debug.Log("StartCoroutine...");
+        StartCoroutine (RedMetricsManager.POST (url, pData, headers, value => wwwLogger(value)));
+    }
+    
+    private void createPlayer (System.Action<WWW> callback)
+    {
+        string url = redMetricsURL + redMetricsPlayer;
+        Dictionary<string, string> headers = new Dictionary<string, string> ();
+        headers.Add ("Content-Type", "application/json");
+        string ourPostData = "{\"type\":"+createPlayerEventType+"}";
+        byte[] pData = System.Text.Encoding.ASCII.GetBytes (ourPostData.ToCharArray ());
+        Debug.Log("StartCoroutine...");
+        StartCoroutine (RedMetricsManager.POST (url, pData, headers, callback));
+    }
+    
+    private void wwwLogger (WWW www)
+    {
+        if (www.error == null) {
+            Debug.LogError ("Success: " + www.text);
+        } else {
+            Debug.LogError ("Error: " + www.error);
+        } 
+    }
+    
+    private string extractPID(WWW www)
+    {
+        string result = null;
+        wwwLogger(www);
+        string trimmed = www.text.Trim ();
+        string[] split1 = trimmed.Split('\n');
+        foreach(string s1 in split1)
+        {
+            Debug.Log(s1);
+            if(s1.Length > 5)
+            {
+                string[] split2 = s1.Trim ().Split(':');
+                foreach(string s2 in split2)
+                {
+                    if(!s2.Equals("id")){
+                        Debug.Log ("id =? "+s2);
+                        result = s2;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    
+    private void trackStart(WWW www)
+    {
+        string pID = extractPID(www);
+        setPlayerID(pID);
+        createEvent(startEventType);
     }
 }
---
+++ Assets/Scripts/GameState/RedMetricsManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RedMetricsManager
+{
+
+    public static IEnumerator GET (string url, System.Action<WWW> callback)
+    {
+        Debug.Log ("GET");
+        WWW www = new WWW (url);
+        yield return www;
+        waitForWWW (www, callback);
+    }
+        
+    public static IEnumerator POST (string url, Dictionary<string,string> post, System.Action<WWW> callback)
+    {
+        Debug.Log ("POST");
+        WWWForm form = new WWWForm ();
+        foreach (KeyValuePair<string,string> post_arg in post) {
+            form.AddField (post_arg.Key, post_arg.Value);
+        }
+            
+        WWW www = new WWW (url, form);
+        return waitForWWW (www, callback);
+    }
+        
+    public static IEnumerator POST (string url, byte[] post, Dictionary<string, string> headers, System.Action<WWW> callback)
+    {
+        Debug.Log ("POST");
+        WWW www = new WWW (url, post, headers);
+        return waitForWWW (www, callback);
+    }
+        
+    private static IEnumerator waitForWWW (WWW www, System.Action<WWW> callback)
+    {
+        Debug.Log ("waitForWWW");
+        float elapsedTime = 0.0f;
+            
+        while (!www.isDone) {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= 10.0f)
+                break;
+            yield return null;
+        }
+            
+        if (!www.isDone || !string.IsNullOrEmpty (www.error)) {
+            string errmsg = string.IsNullOrEmpty (www.error) ? "timeout" : www.error;
+            Debug.LogError ("Load Failed: " + errmsg);
+            callback (null);    // Pass null result.
+            yield break;
+        }
+            
+        callback (www); // Pass retrieved result.
+    }
+}
