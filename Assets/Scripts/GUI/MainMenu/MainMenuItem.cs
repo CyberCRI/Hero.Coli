a7854ee1a893355e29a46364c7d8a830d7e3b496
raphael goujet <raphael.goujet@gmail.com>
2015-05-29 12:10:50 +0200

    started developing the main menu: background; selection through arrow keys

---
+++ Assets/Scripts/GUI/MainMenu/MainMenuItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuItem : MonoBehaviour {
+
+    public string name;
+    
+    public void select() {
+        transform.localScale = new Vector3(transform.localScale.x*2, transform.localScale.y, transform.localScale.z);
+    }
+    public void deselect() {
+        transform.localScale = new Vector3(transform.localScale.x/2, transform.localScale.y, transform.localScale.z);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GUI/MainMenu/MainMenuManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public MainMenuItem[] _items;
+
+    //_currentIndex == -1 means nothing is selected
+    private int _currentIndex = -1;
+
+    private void deselect() {
+        if(_currentIndex >= 0 && _currentIndex < _items.Length) {
+            _items[_currentIndex].deselect();
+            Debug.LogWarning("deselected item "+_currentIndex);
+        } else {
+            Debug.LogWarning("couldn't deselect item "+_currentIndex);
+        }
+        _currentIndex = -1;
+    }
+
+    private bool selectItem(string itemName) {
+        for( int index = 0; index < _items.Length; index++) {
+            if(itemName == _items[index].name) {
+                deselect();
+                _items[index].select();
+                _currentIndex = index;
+                Debug.LogWarning("selected item "+index+" via its name '"+itemName+"'");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool selectItem(int index) {
+        int normalizedIndex = index % _items.Length;
+        if(null != _items[normalizedIndex]) {
+            deselect();
+            _items[normalizedIndex].select();
+            _currentIndex = normalizedIndex;
+            Debug.LogWarning("selected item "+normalizedIndex);
+            return true;
+        }
+        return false;
+    }
+    
+    private bool selectNext() {
+        Debug.LogWarning("selectNext");
+        return selectItem(_currentIndex+1);
+    }
+
+    private bool selectPrevious() {
+        Debug.LogWarning("selectPrevious");
+        return selectItem(_currentIndex-1);
+    }
+
+	// Use this for initialization
+	void Start () {
+        selectItem(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyUp(KeyCode.UpArrow)) {
+            selectPrevious();
+        } else if(Input.GetKeyUp(KeyCode.DownArrow)) {
+            selectNext();
+        }
+	}
+}
