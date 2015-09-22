92f7fcce6a63d5d21d4731322ad282a2a1e3470a
raphael goujet <raphael.goujet@gmail.com>
2015-09-22 18:20:58 +0200

    allowed continuous click to move; added a feedback on click to move; fixed buttons textures atlas; fixed French translation for sound

---
+++ Assets/Scripts/Animations/ClickFeedback.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickFeedback : MonoBehaviour {
+
+    private float duration = 0f;
+    public ParticleEmitter emitter;
+
+    // Use this for initialization
+    void Start () {
+        duration = emitter.maxEnergy;
+        StartCoroutine(selfDestruct());
+    }
+
+    IEnumerator selfDestruct()
+    {
+        yield return new WaitForSeconds(duration);
+        GameObject.Destroy(this.gameObject);
+    }
+}
