578b6c107ee900f93524646965e5db3d4b8e01a0
Raphael Goujet <raphael.goujet@gmail.com>
2015-09-28 18:46:53 +0200

    tried to open new tab instead of opening the mooc webpage on the same tab as the webplayer itself

--- Assets/Scripts/GUI/MainMenu/MOOCOptionsMainMenuItemArray.cs
+++ Assets/Scripts/GUI/MainMenu/MOOCOptionsMainMenuItemArray.cs
@@ -3,14 +3,31 @@ using System.Collections;
 
 public class MOOCOptionsMainMenuItemArray : MainMenuItemArray {
     
-    public GameObject moocOptionsPanel;
+	public GameObject moocOptionsPanel;
 	
 	public static bool isHelp = false;
 	public string url = "http://genius.com/Igem-paris-bettencourt-team-what-are-biobricks-annotated";
 	
-	public void goToMOOC () {			
-		Debug.LogError("MOOCOptionsMainMenuItemArray::goToMOOC attempting to open "+url);
-		Application.OpenURL(url);
+	public void goToMOOC () {	
+		switch (Application.platform) {
+			case RuntimePlatform.WebGLPlayer:
+			case RuntimePlatform.WindowsWebPlayer:
+		case RuntimePlatform.OSXWebPlayer:
+				Debug.LogError ("MOOCOptionsMainMenuItemArray::goToMOOC Webplayer/WebGL attempting to open " + url);
+				Application.ExternalEval("window.open('"+url+"','_blank')");
+				break;
+			case RuntimePlatform.WindowsPlayer: 
+			case RuntimePlatform.OSXPlayer:
+			case RuntimePlatform.WindowsEditor:
+			case RuntimePlatform.OSXEditor:
+				Debug.LogError ("MOOCOptionsMainMenuItemArray::goToMOOC Editor/Standalone attempting to open " + url);
+				Application.OpenURL (url);
+				break;
+			default:
+				Debug.LogError ("MOOCOptionsMainMenuItemArray::goToMOOC default attempting to open " + url);
+				Application.OpenURL (url);
+				break;
+		}
 	}
     
     void OnEnable ()
