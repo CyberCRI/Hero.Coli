d01416c2b228856e069f36d2a3faf192d64e0955
Raphael Goujet <raphael.goujet@gmail.com>
2015-10-01 17:14:02 +0200

    localized the mooc page towards which the user is directed; prepared future mooc urls

--- Assets/Scripts/GUI/MainMenu/LearnMoreOptionsMainMenuItemArray.cs
+++ Assets/Scripts/GUI/MainMenu/LearnMoreOptionsMainMenuItemArray.cs
@@ -6,8 +6,11 @@ public class LearnMoreOptionsMainMenuItemArray : MainMenuItemArray {
 	public GameObject learnMoreOptionsPanel;
 	
 	public static bool isHelp = false;
-	public string url = "http://genius.com/Igem-paris-bettencourt-team-what-are-biobricks-annotated";
-	
+	private string urlKey = "LEARNMORE.LINK.MOOCFULL";
+
+	private string getLocalizedUrl() {
+		return Localization.Localize(urlKey);
+	}
 	public void goToMOOCSameTab() {
 		goToMOOC (false);
 	}
@@ -17,28 +20,28 @@ public class LearnMoreOptionsMainMenuItemArray : MainMenuItemArray {
 
 	private void goToMOOC (bool newTab) {	
 		if (!newTab) {
-			RedMetricsManager.get ().sendEvent (TrackingEvent.GOTOMOOC, new CustomData (CustomDataTag.SAMETAB, url));
-			Application.OpenURL (url);
+			RedMetricsManager.get ().sendEvent (TrackingEvent.GOTOMOOC, new CustomData (CustomDataTag.SAMETAB, getLocalizedUrl()));
+			Application.OpenURL (getLocalizedUrl());
 		} else {
-			RedMetricsManager.get ().sendEvent (TrackingEvent.GOTOMOOC, new CustomData (CustomDataTag.NEWTAB, url));
+			RedMetricsManager.get ().sendEvent (TrackingEvent.GOTOMOOC, new CustomData (CustomDataTag.NEWTAB, getLocalizedUrl()));
 			switch (Application.platform) {
 			case RuntimePlatform.WebGLPlayer:
 			case RuntimePlatform.WindowsWebPlayer:
 			case RuntimePlatform.OSXWebPlayer:
-					Logger.Log ("LearnMoreOptionsMainMenuItemArray::goToMOOC Webplayer/WebGL attempting to open " + url, Logger.Level.WEBPLAYER);
-					Application.ExternalEval ("window.open('" + url + "','_blank')");
-					break;
+				Logger.Log ("LearnMoreOptionsMainMenuItemArray::goToMOOC Webplayer/WebGL attempting to open " + getLocalizedUrl(), Logger.Level.WEBPLAYER);
+				Application.ExternalEval ("window.open('" + getLocalizedUrl() + "','_blank')");
+				break;
 			case RuntimePlatform.WindowsPlayer: 
 			case RuntimePlatform.OSXPlayer:
 			case RuntimePlatform.WindowsEditor:
 			case RuntimePlatform.OSXEditor:
-					Debug.LogError ("LearnMoreOptionsMainMenuItemArray::goToMOOC Editor/Standalone attempting to open " + url);
-					Application.OpenURL (url);
-					break;
+				Debug.LogError ("LearnMoreOptionsMainMenuItemArray::goToMOOC Editor/Standalone attempting to open " + getLocalizedUrl());
+				Application.OpenURL (getLocalizedUrl());
+				break;
 			default:
-					Debug.LogError ("LearnMoreOptionsMainMenuItemArray::goToMOOC default attempting to open " + url);
-					Application.OpenURL (url);
-					break;
+				Debug.LogError ("LearnMoreOptionsMainMenuItemArray::goToMOOC default attempting to open " + getLocalizedUrl());
+				Application.OpenURL (getLocalizedUrl());
+				break;
 			}
 		}
 	}
