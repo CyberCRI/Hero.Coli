4a6bf775529480ed9566f0c9e1cfb05924bb6f7c
Raphael Goujet <raphael.goujet@gmail.com>
2015-12-04 17:18:24 +0100

    added login interface

---
+++ Assets/Scripts/GUI/MainMenu/Login/LoginManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour {
+
+	public Text feedback;
+
+	public void validate(string inputString) {
+		feedback.text = inputString;
+	}
+}
