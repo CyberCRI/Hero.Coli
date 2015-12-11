c65d417b4c6fb8f5e9ea37a74c74fb7a3f9c9048
Raphael Goujet <raphael.goujet@gmail.com>
2015-12-11 18:07:12 +0100

    clicking on the "quit" button after having finished the game redirects the player to the form

--- Assets/Scripts/GameState/EndMainMenuButton.cs
+++ Assets/Scripts/GameState/EndMainMenuButton.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class EndMainMenuButton : ModalButton
 {
+    private string studyURLKey = "STUDY.LEARN.LINK"; 
+    
     public override void press ()
     {
         Logger.Log ("EndMainMenuButton::press()", Logger.Level.INFO);
+        URLOpener.open(studyURLKey, false);        
+        
         //if commented out, causes "enter" in the main menu at the end of the game to restart the game
         ModalManager.unsetModal ();
         GameStateController.get ().endGame();
