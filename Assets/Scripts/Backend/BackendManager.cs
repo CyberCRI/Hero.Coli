00ac45a1cc63ef77081855e4c302c634a839060b
Raphael Goujet <raphael.goujet@gmail.com>
2016-02-22 12:54:07 +0100

    forgotten changes in last commit of branch feature/form on BackEndManager.cs

--- Assets/Scripts/Backend/BackendManager.cs
+++ Assets/Scripts/Backend/BackendManager.cs
@@ -1,22 +1,34 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class BackendManager : MonoBehaviour {
     
     bool isTestGUID = false;
+    private float duration = 2.0f;
     
-    void Start () {
-        isTestGUID = MemoryManager.get().configuration.isTestGUID();
-    }
-	
 	// Update is called once per frame
 	void Update () {
 	   if(Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.RightControl)) {
-           MemoryManager.get().configuration.switchGUID();
+           MemoryManager.get().configuration.switchMetricsGameVersion();
            isTestGUID = MemoryManager.get().configuration.isTestGUID();
-           Debug.LogError("switched to testGUID == "+isTestGUID);
-       }
-       if(isTestGUID) {
-           Logger.Log("test GUID", Logger.Level.ONSCREEN);
+           
+           //display feedback for logging mode
+            GameObject go = new GameObject();
+            GUIText guiText = go.AddComponent<GUIText>();
+            go.transform.position = new Vector3(0.5f,0.5f,0.0f);
+            if(isTestGUID) {
+                guiText.text = "REDMETRICS TEST MODE";
+            } else {
+                guiText.text = "REDMETRICS DEFAULT MODE";
+            }
+            
+            StartCoroutine(waitAndDestroy(go));
        }
 	}
+
+    IEnumerator waitAndDestroy(GameObject go)
+    {
+        yield return new WaitForSeconds(duration);
+        GameObject.Destroy(go);
+    }
 }
