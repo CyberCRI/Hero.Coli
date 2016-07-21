5227bf6a08ebe4d77c51c5444519531822de833b
Raphael Goujet <raphael.goujet@gmail.com>
2016-07-21 12:30:35 +0200

    set up system to switch tutorial version

--- Assets/Scripts/Backend/BackendManager.cs
+++ Assets/Scripts/Backend/BackendManager.cs
@@ -10,26 +10,50 @@ public class BackendManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //logging mode: to test or to production RedMetrics version
         if (Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.RightControl))
         {
             MemoryManager.get().configuration.switchMetricsGameVersion();
             isTestGUID = MemoryManager.get().configuration.isTestGUID();
 
             //display feedback for logging mode
-            GameObject go = new GameObject();
-            GUIText guiText = go.AddComponent<GUIText>();
-            go.transform.position = new Vector3(0.5f, 0.5f, 0.0f);
+            string msg;
             if (isTestGUID)
             {
-                guiText.text = "REDMETRICS TEST MODE";
+                msg = "REDMETRICS TEST MODE";
             }
             else
             {
-                guiText.text = "REDMETRICS DEFAULT MODE";
+                msg = "REDMETRICS DEFAULT MODE";
             }
 
-            StartCoroutine(waitAndDestroy(go));
+            StartCoroutine(waitAndDestroy(createMessage(msg)));
         }
+        //toggle tutorial mode
+        else if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            string msg;
+            if(MemoryManager.get().configuration.tutorialMode == GameConfiguration.TutorialMode.START0FLAGELLUMHORIZONTALTRANSFER)
+            {
+                MemoryManager.get().configuration.tutorialMode = GameConfiguration.TutorialMode.START1FLAGELLUM4BRICKS;
+                msg = "MODE = 1FLAGELLUM 4BRICKS";
+            }
+            else
+            {
+                 MemoryManager.get().configuration.tutorialMode = GameConfiguration.TutorialMode.START0FLAGELLUMHORIZONTALTRANSFER;
+                 msg = "MODE = START0FLAGELLUM HORIZONTALTRANSFER";
+            }
+            StartCoroutine(waitAndDestroy(createMessage(msg)));
+        }
+    }
+    
+    public GameObject createMessage(string msg)
+    {
+        GameObject go = new GameObject();
+        GUIText guiText = go.AddComponent<GUIText>();
+        go.transform.position = new Vector3(0.5f, 0.5f, 0.0f);
+        guiText.text = msg;
+        return go;
     }
 
     IEnumerator waitAndDestroy(GameObject go)
