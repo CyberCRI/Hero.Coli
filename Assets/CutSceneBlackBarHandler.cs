2fe12d40a90b112d06e51e37f688ea15102831eb
Guillaume Laruelle <g.laruelle@student.isartdigital.com>
2016-07-28 19:09:49 +0200

    Added CutScenes Black Bar + handler

---
+++ Assets/CutSceneBlackBarHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutSceneBlackBarHandler : MonoBehaviour {
+
+    [SerializeField]
+    private Animator _animTop;
+    [SerializeField]
+    private Animator _animBottom;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            CloseBar(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            CloseBar(false);
+        }
+
+    }
+
+    public void CloseBar(bool value)
+    {
+        _animBottom.SetBool("Close", value);
+        _animTop.SetBool("Close", value);
+    }
+}
