42d5a7fd1a7a0f65644e87d713ad5c3a4a6249f9
Guillaume Laruelle <g.laruelle@student.isartdigital.com>
2016-07-29 18:25:19 +0200

    Added Advisor to Focus Mask

---
+++ Assets/Advisor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Advisor : MonoBehaviour {
+
+    [SerializeField]
+    private Text _dynamicText;
+    [SerializeField]
+    private Transform _positionTop;
+    [SerializeField]
+    private Transform _positionBottom;
+    [SerializeField]
+    private GameObject _nanoBot;
+
+    public void setUpNanoBot(bool top, string text)
+    {
+        if (top == true)
+        {
+            _nanoBot.transform.position = _positionTop.position;
+        }
+        if (top == false)
+        {
+            _nanoBot.transform.position = _positionBottom.position;
+        }
+
+        _dynamicText.text = text;
+    }
+
+    public void setUpNanoBot(bool top)
+    {
+        if (top == true)
+        {
+            _nanoBot.transform.position = _positionTop.position;
+        }
+        if (top == false)
+        {
+            _nanoBot.transform.position = _positionBottom.position;
+        }
+    }
+
+    public void setUpNanoBot(string text)
+    {
+        _dynamicText.text = text;
+    }
+}
