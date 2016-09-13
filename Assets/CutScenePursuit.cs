c891fa3c2ef9b27a3b2496d295ad95c217092e8d
Guillaume Laruelle <g.laruelle@student.isartdigital.com>
2016-09-13 19:14:36 +0200

    Added CutSceneZone8

--- Assets/CutScenePursuit.cs
+++ Assets/CutScenePursuit.cs
@@ -13,11 +13,6 @@ public class CutScenePursuit : CutScene {
     [SerializeField]
     private Transform _wayPoint2;
     private int _iteration = 0;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/CutSceneZone8.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CutSceneZone8 : CutScene {
+
+    [SerializeField]
+    private GameObject _dummy;
+    private int _collisionIteration = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void initialize()
+    {
+        
+    }
+
+    public override void startCutScene()
+    {
+        
+    }
+
+    public override void endCutScene()
+    {
+        _cellControl.freezePlayer(false);
+        this.transform.parent.gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Door")
+        {
+            if (_collisionIteration == 0)
+            {
+                start();
+                _collisionIteration++;
+            }
+            else
+            {
+                Destroy(_dummy.GetComponent<RotationUpdate>());
+                Destroy(_dummy.GetComponent<PlatformMvt>());
+                _dummy.GetComponent<iTweenEvent>().enabled = true;
+                StartCoroutine(WaitForEnd());
+            }
+        }
+    }
+
+    IEnumerator WaitForEnd()
+    {
+        yield return new WaitForSeconds(3f);
+        end();
+        yield return null;
+    }
+}
