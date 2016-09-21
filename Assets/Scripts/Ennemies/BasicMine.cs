6c396b5a2f14c907ae020afab3c173ff3ff24dfc
Raphael Goujet <raphael.goujet@gmail.com>
2016-09-21 12:37:16 +0200

    fixed Mine vs NPCs' logic; fixed mine prefab and instances in Tutorial1; improved mine explosion particle system

--- Assets/Scripts/Ennemies/BasicMine.cs
+++ Assets/Scripts/Ennemies/BasicMine.cs
@@ -2,33 +2,21 @@ using UnityEngine;
 
 public class BasicMine : ResettableMine
 {
-    private bool _detonated = false;
-
-    private bool _isReseting = false;
-
-    private bool _first = true;
-
-    public void detonate(bool reseting)
+    public void detonate()
     {
-        Debug.Log("self " + this.gameObject.name + " detonates");
-        _isReseting = reseting;
         MineManager.get().detonate(this);
-        _detonated = true;
     }
-
-    public bool isDetonated() { return _detonated; }
 
     void OnCollisionEnter(Collision collision)
     {
-		Debug.Log("colliding with " + collision.gameObject.name);
+        detonate();
         if (collision.gameObject.name == Hero.gameObjectName)
         {
-            detonate(true);
-            collision.gameObject.GetComponent<Hero>().getLifeManager().setSuddenDeath(true);
+            Hero.get().getLifeManager().setSuddenDeath(true);
         }
         else if (collision.gameObject.tag == "NPC")
         {
-            detonate(false);
+            collision.gameObject.GetComponent<DeathDummy>().startDeath();
         }
     }
 }
