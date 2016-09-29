6e3c098bd72e5500a829c3d53a9c4d7097633bb3
Guillaume Laruelle <g.laruelle@student.isartdigital.com>
2016-09-29 19:21:43 +0200

    Fix Cutscene Black Bar appearing at the start of the game

--- Assets/Scripts/CutScene/CutSceneBlackBarHandler.cs
+++ Assets/Scripts/CutScene/CutSceneBlackBarHandler.cs
@@ -7,6 +7,12 @@ public class CutSceneBlackBarHandler : MonoBehaviour {
     [SerializeField]
     private Animator _bottomAnimation;
 	
+    void Awake()
+    {
+        /*_bottomAnimation.speed = 0;
+        _topAnimation.speed = 0;*/
+    }
+
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetKeyDown(KeyCode.Keypad5))
@@ -23,6 +29,8 @@ public class CutSceneBlackBarHandler : MonoBehaviour {
     public void closeBar(bool close)
     {
         _bottomAnimation.SetBool("Close", close);
+        _bottomAnimation.SetBool("Start", false);
         _topAnimation.SetBool("Close", close);
+        _topAnimation.SetBool("Start", false);
     }
 }
