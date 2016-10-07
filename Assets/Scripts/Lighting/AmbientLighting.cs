2f11faa34d5cbc1d5cc783494c7dffa9bc392a35
Raphael Goujet <raphael.goujet@gmail.com>
2016-10-07 19:14:22 +0200

    fixed bug on GFP: bad initialization, bad behavior when on edge of LightEmitter(s)

--- Assets/Scripts/Lighting/AmbientLighting.cs
+++ Assets/Scripts/Lighting/AmbientLighting.cs
@@ -86,6 +86,11 @@ public class AmbientLighting : MonoBehaviour
         Color color = _backgroundBloodRenderer.material.color;
         color.a = 0;
         _backgroundBloodRenderer.material.color = color;
+
+        _phenoLight.enabled = true;
+        _spotLight.enabled = true;
+        _blackLightSpotLight.enabled = false;
+
         saveCurrentLighting();
     }
 
--- Assets/Scripts/Phenotypes/PhenoLight.cs
+++ Assets/Scripts/Phenotypes/PhenoLight.cs
@@ -7,29 +7,31 @@ using System;
  
  
  */
-public class PhenoLight : Phenotype {
+public class PhenoLight : Phenotype
+{
 
-  // TODO use a LinkedList to manage overlapping light sources
+    // TODO use a LinkedList to manage overlapping light sources
 
     [SerializeField]
     private Light _phenoLight, _spotLight, _blackLightSpotLight;
-    
-    private bool _active = false;
+
+    public bool _active = false;
     private string _fluorescenceProtein;
     private bool _isSystemTriggered = false;
     private Molecule _mol = null;
     private TriggeredLightEmitter _triggered;
+    private TriggeredLightEmitter _lm;
 
     private const float _maxConcentration = 270f, _maxValue = 8f;
     private const float _steepness = _maxValue / _maxConcentration;
 
-  //! Called at the beginning
-  public override void StartPhenotype()
-  {
-      
-  }
+    //! Called at the beginning
+    public override void StartPhenotype()
+    {
+        turnLightOff();
+    }
 
-  	/*!
+    /*!
     \brief This function is called as Update in Monobehaviour.
     \details This function is called in the Phenotype class in the Update function
     This function should be implemented and all the graphical action has to be implemented in it.
@@ -37,23 +39,24 @@ public class PhenoLight : Phenotype {
    */
     public override void UpdatePhenotype()
     {
-        if(_active)
+        if (_active)
         {
-            if (_mol == null) {
-                _mol = ReactionEngine.getMoleculeFromName(_fluorescenceProtein, _molecules);   
+            if (_mol == null)
+            {
+                _mol = ReactionEngine.getMoleculeFromName(_fluorescenceProtein, _molecules);
             }
-                
+
             float intensity = Mathf.Min(_steepness * _mol.getConcentration(), _maxValue);
-    
+
             _phenoLight.intensity = intensity;
             _spotLight.intensity = intensity;
             _blackLightSpotLight.intensity = intensity;
-            
-            if(null != _triggered)
+
+            if (null != _triggered)
             {
-                if(_mol.getConcentration() > _triggered.threshold)
+                if (_mol.getConcentration() > _triggered.threshold)
                 {
-                    if(!_isSystemTriggered)
+                    if (!_isSystemTriggered)
                     {
                         turnLightOn();
                     }
@@ -65,58 +68,77 @@ public class PhenoLight : Phenotype {
                 // The concentration fell below the threshold and the light must
                 // consequently be switched off
                 else if (_isSystemTriggered)
-                {                    
+                {
                     turnLightOff();
                 }
             }
         }
     }
-	
-	void OnTriggerEnter(Collider col)
+
+    void OnTriggerEnter(Collider col)
     {
+        // Debug.Log("OnTriggerEnter " + col.name);
         // Prepares the future triggering of the light
-		TriggeredLightEmitter lm = col.gameObject.GetComponent<TriggeredLightEmitter>();
-		if(null != lm)
-        {
-            _triggered = lm;
-            _fluorescenceProtein = lm.protein;
-            _active = !String.IsNullOrEmpty(_fluorescenceProtein);
-		}
- 	}
-     
-	void OnTriggerExit(Collider col)
+        manageInactiveCollision(col);
+    }
+
+    private void manageInactiveCollision(Collider col)
     {
-		TriggeredLightEmitter lm = col.gameObject.GetComponent<TriggeredLightEmitter>();
-		if(null != lm){
+        if (!_active)
+        {
+            _lm = col.gameObject.GetComponent<TriggeredLightEmitter>();
+            if (null != _lm)
+            {
+                _triggered = _lm;
+                _fluorescenceProtein = _lm.protein;
+                _mol = null;
+                _active = !String.IsNullOrEmpty(_fluorescenceProtein);
+                // Debug.Log("manageInactiveCollision _active=" + _active);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        // Debug.Log("OnTriggerExit " + col.name);
+        _lm = col.gameObject.GetComponent<TriggeredLightEmitter>();
+        if (null != _lm)
+        {
             turnLightOff();
             _triggered = null;
             _fluorescenceProtein = "";
+            _mol = null;
             _active = false;
-		}
-	}
-    
+            // Debug.Log("OnTriggerExit _active=" + _active);
+        }
+    }
+    void OnTriggerStay(Collider col)
+    {
+        manageInactiveCollision(col);
+    }
+
     private void turnLightOff()
     {
-        if(_triggered)
+        if (_triggered)
         {
             _triggered.triggerExit();
         }
         _isSystemTriggered = false;
-        if(_phenoLight)
+        if (_phenoLight)
         {
             _phenoLight.gameObject.SetActive(false);
             _spotLight.gameObject.SetActive(false);
             _blackLightSpotLight.gameObject.SetActive(false);
         }
     }
-    
+
     private void turnLightOn()
     {
-        if(_triggered)
+        if (_triggered)
         {
             _triggered.triggerStart();
             _isSystemTriggered = true;
-            
+
             _phenoLight.gameObject.SetActive(true);
             _phenoLight.color = _triggered.colorTo;
 
