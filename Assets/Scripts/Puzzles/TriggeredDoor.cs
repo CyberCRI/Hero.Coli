6578fbc6a745753b34c433d088d7aee7d6ddd0c8
Raphael Goujet <raphael.goujet@gmail.com>
2016-11-25 15:03:16 +0100

    fixed #411 "First door of zone 7 has erratic behavior" and fixed #424 "First door after race close a bit too fast"

--- Assets/Scripts/Puzzles/TriggeredDoor.cs
+++ Assets/Scripts/Puzzles/TriggeredDoor.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class TriggeredDoor : TriggeredBehaviour
 {
@@ -10,54 +11,155 @@ public class TriggeredDoor : TriggeredBehaviour
     private float closeDelay = 0f;
     [SerializeField]
     private float minDistance = 0.1f;
-    private bool _isTriggered = false;
+
+    // TODO switch/channeling mode
+    // // If true, switch mode: the door opens completely however short the entering collision was.
+    // // Otherwise, channeling mode: the player has to wait in the collider for the door to completely open.
+    // [SerializeField]
+    // private bool _isSwitchMode = true;
+
     private Vector3 _origin;
+    private bool _isDoorOpening = false;
+    private bool _isDoorClosing = false;
+    private bool _isBeingTriggered = false;
+    private const string openDoorITweenName = "openDoor";
+    private const string closeDoorITweenName = "closeDoor";
+    private IEnumerator waitCoroutine;
 
     void Start()
     {
         _origin = transform.position;
     }
 
-    void Update()
+    Hashtable generateOpenHash()
     {
-        if (_isTriggered && transform.position == _origin)
-        {
-            _isTriggered = false;
-        }
-    }
-
-    private void trigger(string _name)
-    {
-        if (!_isTriggered && Vector3.Distance(transform.position, moveTo.position) > minDistance)
-        {
-            iTween.MoveTo(gameObject, iTween.Hash(
+        return iTween.Hash(
                 "position", moveTo,
                 "speed", speed,
                 "easetype", iTween.EaseType.easeInOutQuad,
-                "name", _name
-            ));
-            _isTriggered = true;
+                "oncomplete", "onDoorOpened",
+                "name", openDoorITweenName
+            );
+    }
+
+    Hashtable generateCloseHash()
+    {
+        return iTween.Hash(
+                "position", _origin,
+                "speed", speed,
+                "easetype", iTween.EaseType.easeInOutQuad,
+                "oncomplete", "onDoorClosed",
+                "name", closeDoorITweenName);
+    }
+
+    void openDoor()
+    {
+        // Debug.Log(this.GetType() + " openDoor");
+        _isDoorOpening = true;
+        _isDoorClosing = false;
+        iTween.MoveTo(gameObject, generateOpenHash());
+    }
+
+    void onDoorOpened()
+    {
+        // Debug.Log(this.GetType() + " onDoorOpened");
+        _isDoorOpening = false;
+        // delay time counts as closing time
+        _isDoorClosing = !_isBeingTriggered;
+        if (_isDoorClosing)
+        {
+            if (null != waitCoroutine)
+            {
+                // Debug.Log(this.GetType() + " onDoorOpened stops waitCoroutine");
+                StopCoroutine(waitCoroutine);
+            }
+            waitCoroutine = waitAndCloseDoor();
+            StartCoroutine(waitCoroutine);
         }
+    }
+
+    IEnumerator waitAndCloseDoor()
+    {
+        // Debug.Log(this.GetType() + " waitAndCloseDoor");
+        _isDoorOpening = false;
+        // delay time counts as closing time
+        _isDoorClosing = true;
+        yield return new WaitForSeconds(closeDelay);
+        closeDoor();
+        yield return null;
+    }
+
+    private void closeDoor()
+    {
+        // Debug.Log(this.GetType() + " closeDoor");
+        _isDoorOpening = false;
+        _isDoorClosing = true;
+        iTween.MoveTo(gameObject, generateCloseHash());
+    }
+
+    void onDoorClosed()
+    {
+        // Debug.Log(this.GetType() + " onDoorClosed");
+        _isDoorOpening = false;
+        _isDoorClosing = false;
     }
 
     public override void triggerStart()
     {
-        trigger("triggerStart");
+        // Debug.Log(this.GetType() + " triggerStart");
+
+        _isBeingTriggered = true;
+        if(_isDoorClosing)
+        {
+            // halt the closing
+            // coroutine
+            if (null != waitCoroutine)
+            {
+                // Debug.Log(this.GetType() + " triggerStart stops waitCoroutine");
+                StopCoroutine(waitCoroutine);
+            }
+            // actual closing
+            // Debug.Log(this.GetType() + " triggerStart searches iTween " + closeDoorITweenName);
+            iTween[] itweens = gameObject.GetComponents(typeof(iTween)) as iTween[];
+            if(null != itweens)
+            {
+                foreach(iTween it in itweens)
+                {
+                    if (closeDoorITweenName == it.name)
+                    {
+                        Destroy(it);
+                        // Debug.Log(this.GetType() + " triggerStart stops iTween " + closeDoorITweenName);
+                        break;
+                    }
+                }
+            }
+            openDoor();
+        }
+        else if (!_isDoorOpening)
+        {
+            openDoor();
+        }
     }
 
     public override void triggerStay()
     {
-        // trigger("triggerStay");
     }
 
     public override void triggerExit()
     {
-        iTween.MoveTo(gameObject, iTween.Hash(
-        "position", _origin,
-        "speed", speed,
-        "easetype", iTween.EaseType.easeInOutQuad,
-        "delay", closeDelay,
-        "name", "triggerExit"));
-        _isTriggered = false;
+        // Debug.Log(this.GetType() + " triggerExit");
+
+        _isBeingTriggered = false;
+
+        if (!_isDoorOpening && !_isDoorClosing)
+        {
+            if (null != waitCoroutine)
+            {
+                // Debug.Log(this.GetType() + " triggerExit stops waitCoroutine");
+                StopCoroutine(waitCoroutine);
+            }
+            waitCoroutine = waitAndCloseDoor();
+            StartCoroutine(waitCoroutine);
+        }
     }
 }
