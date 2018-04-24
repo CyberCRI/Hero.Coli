4ac23caa8444790cb96d5659538bb5fe5acdfec1
raphael goujet <raphael.goujet@gmail.com>
2018-04-24 10:35:21 +0200

    fixed appearance of plasmids; fixed ampicillin lighting; fixed PulsingLight in general

--- Assets/Scripts/Lighting/PulsingLight.cs
+++ Assets/Scripts/Lighting/PulsingLight.cs
@@ -5,15 +5,15 @@ public class PulsingLight : MonoBehaviour
 {
 
     [SerializeField]
-    private bool _tweekIntensity = true;
+    private bool _tweakIntensity = true;
     [SerializeField]
-    private bool _tweekRange = false;
+    private bool _tweakRange = false;
     private Light _ownLight;
     [SerializeField]
-    private float _speedIntensity;
+    private float _intensitySpeed;
     [SerializeField]
-    private float _speedRange;
-    private bool _way = false;
+    private float _rangeSpeed;
+    private bool _isLightIncreasing = true;
     [SerializeField]
     private float _minIntensity = 1;
     [SerializeField]
@@ -23,59 +23,58 @@ public class PulsingLight : MonoBehaviour
     [SerializeField]
     private float _maxRange;
 
-    private float _speedIntensityPercent;
-    private float _speedRangePercent;
-
     void Awake()
     {
-        _speedIntensityPercent = _speedIntensity / 100;
-        _speedRangePercent = _speedRange / 100;
         _ownLight = this.GetComponent<Light>();
 
-        if (_tweekIntensity)
+        if (_tweakIntensity)
             _ownLight.intensity = _minIntensity;
-        if (_tweekRange)
+        if (_tweakRange)
             _ownLight.range = _minRange;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!_way)
+        if (_isLightIncreasing)
         {
-            if (_tweekIntensity)
+            if (_tweakIntensity)
             {
-                _ownLight.intensity += _speedIntensityPercent;
-                if (_ownLight.intensity >= _maxIntensity)
+                _ownLight.intensity += _intensitySpeed * Time.deltaTime;
+                if (_ownLight.intensity > _maxIntensity)
                 {
-                    _way = !_way;
+                    _ownLight.intensity = _maxIntensity;
+                    _isLightIncreasing = !_isLightIncreasing;
                 }
             }
-            if (_tweekRange)
+            if (_tweakRange)
             {
-                _ownLight.range += _speedRangePercent;
-                if (_ownLight.range >= _maxRange)
+                _ownLight.range += _rangeSpeed * Time.deltaTime;
+                if (_ownLight.range > _maxRange)
                 {
-                    _way = !_way;
+                    _ownLight.range = _maxRange;
+                    _isLightIncreasing = !_isLightIncreasing;
                 }
             }
         }
         else
         {
-            if (_tweekIntensity)
+            if (_tweakIntensity)
             {
-                _ownLight.intensity -= _speedIntensityPercent;
-                if (_ownLight.intensity <= _minIntensity)
+                _ownLight.intensity -= _intensitySpeed * Time.deltaTime;
+                if (_ownLight.intensity < _minIntensity)
                 {
-                    _way = !_way;
+                    _ownLight.intensity = _minIntensity;
+                    _isLightIncreasing = !_isLightIncreasing;
                 }
             }
-            if (_tweekRange)
+            if (_tweakRange)
             {
-                _ownLight.range -= _speedRangePercent;
-                if (_ownLight.range <= _minRange)
+                _ownLight.range -= _rangeSpeed * Time.deltaTime;
+                if (_ownLight.range < _minRange)
                 {
-                    _way = !_way;
+                    _ownLight.range = _minRange;
+                    _isLightIncreasing = !_isLightIncreasing;
                 }
             }
         }
